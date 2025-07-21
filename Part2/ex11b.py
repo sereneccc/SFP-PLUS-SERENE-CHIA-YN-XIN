13017f01490396a64d0edae896cbc02f813019ad
sereneccc <ynx.chia@gmail.com>
2025-07-21 13:42:39 +0000

    done

--- Part2/ex11b.py
+++ Part2/ex11b.py
@@ -1,1 +1,17 @@
-print(calculate(10, "+", 10)) #20
+def calculate(num1, operator, num2):
+    if operator == "+":
+        return num1 +num2
+    elif operator == "-":
+        return num1 -  num2
+    elif operator == "*":
+        return num1 * num2
+    elif operator == "/":
+        return num1 / num2
+    else:
+        return "Invalid operator"
+print(calculate(10, "+", 10))
+print(calculate(10, "-", 10))
+print(calculate(10, "*", 10))
+print(calculate(10, "/", 10))
+    
+    
