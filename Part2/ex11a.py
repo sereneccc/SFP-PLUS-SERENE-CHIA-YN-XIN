ee3da45916222243014862af5237275e6c9dc8b5
sereneccc <ynx.chia@gmail.com>
2025-07-21 11:46:36 +0000

    done

---
+++ Part2/ex11a.py
@@ -0,0 +1,8 @@
+# Define the function add_two
+def add_two(number):
+    return number + 2
+# Call the function directly and print
+print(add_two(4))
+# Store the result in a variable and print
+result = add_two(8)
+print(result)
